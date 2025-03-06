6b170a2aed1dc1d3681190d9c16e86238c50c5d4
Luis Gabriel García Valdés <dev.nether@gmail.com>
2025-03-05 19:24:30 -0500

    Added login endpoint

--- secondChance-backend/routes/authRouter.js
+++ secondChance-backend/routes/authRouter.js
@@ -1,51 +1,85 @@
-const express = require('express');
-const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const connectToDatabase = require('../models/db');
+const express = require("express");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const connectToDatabase = require("../models/db");
 const router = express.Router();
-const dotenv = require('dotenv');
-const pino = require('pino'); 
+const dotenv = require("dotenv");
+const pino = require("pino");
 dotenv.config();
 
-const logger = pino(); 
+const logger = pino();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-router.post('/register', async (req, res) => {
-    try {
-      const db = await connectToDatabase();
-      const collection = db.collection("users");
-      const existingEmail = await collection.findOne({ email: req.body.email });
+router.post("/register", async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection("users");
+    const existingEmail = await collection.findOne({ email: req.body.email });
 
-        if (existingEmail) {
-            logger.error('Email id already exists');
-            return res.status(400).json({ error: 'Email id already exists' });
-        }
-
-        const salt = await bcryptjs.genSalt(10);
-        const hash = await bcryptjs.hash(req.body.password, salt);
-        const email=req.body.email;
-        const newUser = await collection.insertOne({
-            email: req.body.email,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            password: hash,
-            createdAt: new Date(),
-        });
-
-        const payload = {
-            user: {
-                id: newUser.insertedId,
-            },
-        };
-
-        const authtoken = jwt.sign(payload, JWT_SECRET);
-        logger.info('User registered successfully');
-        res.json({ authtoken,email });
-    } catch (e) {
-        logger.error(e);
-        return res.status(500).send('Internal server error');
+    if (existingEmail) {
+      logger.error("Email id already exists");
+      return res.status(400).json({ error: "Email id already exists" });
     }
+
+    const salt = await bcryptjs.genSalt(10);
+    const hash = await bcryptjs.hash(req.body.password, salt);
+    const email = req.body.email;
+    const newUser = await collection.insertOne({
+      email: req.body.email,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      password: hash,
+      createdAt: new Date(),
+    });
+
+    const payload = {
+      user: {
+        id: newUser.insertedId,
+      },
+    };
+
+    const authtoken = jwt.sign(payload, JWT_SECRET);
+    logger.info("User registered successfully");
+    res.json({ authtoken, email });
+  } catch (e) {
+    logger.error(e);
+    return res.status(500).send("Internal server error");
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+    const collection = db.collection("users");
+    const user = await collection.findOne({ email: req.body.email });
+
+    if (user) {
+      let result = await bcryptjs.compare(req.body.password, user.password);
+
+      if (!result) {
+        logger.error("Wrong credentials");
+        return res.status(404).json({ error: "Wrong credentials" });
+      }
+      const username = user.username;
+      const email = user.email;
+      const payload = {
+        user: {
+          id: newUser.insertedId,
+        },
+      };
+
+      const authtoken = jwt.sign(payload, JWT_SECRET);
+      logger.info("User logged in successfully");
+      return res.status(200).json({ authtoken, username, email });
+    } else {
+      logger.error("User not found");
+      return res.status(404).json({ error: "User not found" });
+    }
+  } catch (e) {
+    logger.error(e);
+    return res.status(500).send("Internal server error");
+  }
 });
 
 module.exports = router;
