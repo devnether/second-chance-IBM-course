431a07939d9b130a07a48c821069f8416de4fd93
Luis Gabriel García Valdés <dev.nether@gmail.com>
2025-03-05 19:39:10 -0500

    Added update endpoint

--- secondChance-backend/routes/authRouter.js
+++ secondChance-backend/routes/authRouter.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const connectToDatabase = require("../models/db");
@@ -78,6 +79,53 @@ router.post("/login", async (req, res) => {
     }
   } catch (e) {
     logger.error(e);
+    return res.status(500).send("Internal server error");
+  }
+});
+
+router.put("/update", async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    logger.error("Validation errors in update request", errors.array());
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const email = req.header.email;
+    if (!email) {
+      logger.error("Email not found in the request headers");
+      return res
+        .status(400)
+        .json({ error: "Email not found in the request headers" });
+    }
+    const db = await connectToDatabase();
+    const collection = db.collection("users");
+    const user = await collection.findOne({ email: email });
+
+    if (!existingUser) {
+      logger.error("User not found");
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    user.firstName = req.body.firstName;
+    existingUser.updatedAt = new Date();
+
+    const updatedUser = await collection.findOneAndUpdate(
+      { email },
+      { $set: existingUser },
+      { returnDocument: "after" }
+    );
+    const payload = {
+      user: {
+        id: updatedUser.insertedId,
+      },
+    };
+
+    const authtoken = jwt.sign(payload, JWT_SECRET);
+    logger.info("User updated successfully");
+
+    res.json(authtoken);
+  } catch (e) {
     return res.status(500).send("Internal server error");
   }
 });
