61765503eaaa20b80b073fcad4a1a88402a95e6b
Luis Gabriel García Valdés <dev.nether@gmail.com>
2025-03-18 19:25:18 -0400

    FIXED M INOR ISSUES

--- sentiment/index.js
+++ sentiment/index.js
@@ -1,8 +1,9 @@
-require('dotenv').config();
-const express = require('express');
-const axios = require('axios');
-const logger = require('./logger');
-const expressPino = require('express-pino-logger')({ logger });
+require("dotenv").config();
+const express = require("express");
+// eslint-disable
+const axios = require("axios");
+const logger = require("./logger");
+const expressPino = require("express-pino-logger")({ logger });
 const natural = require("natural");
 
 const app = express();
@@ -11,38 +12,39 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(expressPino);
 
-app.post('/sentiment', async (req, res) => {
-    const { sentence } = req.query;
+app.post("/sentiment", async (req, res) => {
+  const { sentence } = req.query;
 
+  if (!sentence) {
+    logger.error("No sentence provided");
+    return res.status(400).json({ error: "No sentence provided" });
+  }
 
-    if (!sentence) {
-        logger.error('No sentence provided');
-        return res.status(400).json({ error: 'No sentence provided' });
+  const Analyzer = natural.SentimentAnalyzer;
+  const stemmer = natural.PorterStemmer;
+  const analyzer = new Analyzer("English", stemmer, "afinn");
+
+  try {
+    const analysisResult = analyzer.getSentiment(sentence.split(" "));
+
+    let sentiment = "neutral";
+
+    if (analysisResult < 0) {
+      sentiment = "negative";
+    } else if (analysisResult > 0.33) {
+      sentiment = "positive";
     }
-
-    const Analyzer = natural.SentimentAnalyzer;
-    const stemmer = natural.PorterStemmer;
-    const analyzer = new Analyzer("English", stemmer, "afinn");
-
-    try {
-        const analysisResult = analyzer.getSentiment(sentence.split(' '));
-
-        let sentiment = "neutral";
-
-        if (analysisResult < 0) {
-            sentiment = "negative";
-        } else if (analysisResult > 0.33) {
-            sentiment = "positive";
-        }
-        logger.info(`Sentiment analysis result: ${analysisResult}`);
-        res.status(200).json({ sentimentScore: analysisResult, sentiment: sentiment });
-    } catch (error) {
-        logger.error(`Error performing sentiment analysis: ${error}`);
-        res.status(500).json({ message: 'Error performing sentiment analysis' });
-    }
+    logger.info(`Sentiment analysis result: ${analysisResult}`);
+    res
+      .status(200)
+      .json({ sentimentScore: analysisResult, sentiment: sentiment });
+  } catch (error) {
+    logger.error(`Error performing sentiment analysis: ${error}`);
+    res.status(500).json({ message: "Error performing sentiment analysis" });
+  }
 });
 
 // Start the server
 app.listen(port, () => {
-    logger.info(`Server running on port ${port}`);
+  logger.info(`Server running on port ${port}`);
 });
