16ca60bdca48d2736c9238b9d8d607dd5d3ceebf
Luis Gabriel García Valdés <dev.nether@gmail.com>
2025-03-17 20:18:13 -0400

    fixing minor issies

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -6,7 +6,7 @@ const pinoLogger = require("./logger");
 
 const connectToDatabase = require("./models/db");
 
-//const { loadData } = require("./util/import-mongo/index");
+
 const secondChanceItemsRoutes = require("./routes/secondChanceItemsRoutes");
 const searchRoutes = require("./routes/searchRoutes");
 const authRoutes = require("./routes/authRoutes");
